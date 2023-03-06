93b497abce01ae21e1f34e2f5dd68a88f79cdf22
DreaM4ker <117078170+DreaM4ker@users.noreply.github.com>
2023-03-06 13:04:31 +0100

    Add files via upload

--- intro_c/includes.h
+++ intro_c/includes.h
@@ -6,15 +6,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
-
-#define VRAI 1
-#define FAUX 0
-typedef int BOOLEAN;
-
 typedef struct my_lst {
     int nbr;
     struct my_lst *next;
 } type_lst;
+
 
 #endif
